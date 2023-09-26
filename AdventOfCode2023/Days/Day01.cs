41f2a7b7f5a7b29fa31019d38e0003a41b927fa4
Josiah Bradbury <herrozerro@gmail.com>
2023-09-26 08:08:01 -0500

    Initial Commit

---
+++ AdventOfCode2023/Days/Day01.cs
@@ -0,0 +1,27 @@
+using AdventOfCode2023.Utilities;
+
+namespace AdventOfCode2023.Days;
+
+public class Day01 : IDay
+{
+    
+    public void SolvePart1(bool isTest = false)
+    {
+        var filename = $"Days/{GetType().Name}/input{(isTest ? ".Test" : "")}.txt";
+        var input = FileUtility.ReadLinesFromFile(filename);
+        
+        
+        
+        
+    }
+
+    public void SolvePart2(bool isTest = false)
+    {
+        var filename = $"Days/{GetType().Name}/input{(isTest ? ".Test" : "")}.txt";
+        var input = FileUtility.ReadLinesFromFile(filename);
+        
+        
+        
+        
+    }
+}
---
+++ AdventOfCode2023/Days/IDay.cs
@@ -0,0 +1,7 @@
+namespace AdventOfCode2023.Days;
+
+public interface IDay
+{
+    public void SolvePart1(bool isTest = false);
+    public void SolvePart2(bool isTest = false);
+}
